d5ea3d40af4bdd3bc1e5bcd6b48d178ae57e4440
Le-Vu-Hoang <bx1536hoang@gmail.com>
2025-03-23 18:45:52 +0700

    model v1

---
+++ detect.py
@@ -0,0 +1,40 @@
+from ultralytics import YOLO
+import cv2
+import os
+
+model = YOLO("models/best.pt")
+
+def detect_objects(img):
+    results = model(img, conf=0.1)[0]
+
+    # Vẽ bounding box
+    for box in results.boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        conf = box.conf[0].item() 
+        cls = int(box.cls[0])
+
+        label = f"{model.names[cls]}: {conf:.2f}"
+        color = (51, 102, 0)
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
+        cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    return img
+
+def process_video(input_path, output_path):
+    cap = cv2.VideoCapture(input_path)
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        processed_frame = detect_objects(frame)
+        out.write(processed_frame)
+
+    cap.release()
+    out.release()
---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, UploadFile, File, Response
+from fastapi.responses import FileResponse
+import numpy as np
+import cv2
+import shutil
+import os
+from detect import detect_objects, process_video
+
+app = FastAPI()
+UPLOAD_FOLDER = "uploads"
+OUTPUT_FOLDER = "outputs"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+
+@app.post("/detect/")
+async def detect(file: UploadFile = File(...)):
+    contents = await file.read()
+    nparr = np.frombuffer(contents, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    output_img = detect_objects(img)
+    _, img_encoded = cv2.imencode(".jpg", output_img)
+    return Response(content=img_encoded.tobytes(), media_type="image/jpeg")
+
+@app.post("/detect-video/")
+async def detect_video_api(file: UploadFile = File(...)):
+    video_path = f"{UPLOAD_FOLDER}/{file.filename}"
+    output_path = f"{OUTPUT_FOLDER}/processed_{file.filename}"
+    with open(video_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    process_video(video_path, output_path)
+
+    return FileResponse(output_path, media_type="video/mp4", filename=f"processed_{file.filename}")
