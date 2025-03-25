ae851ebc92e382b81721572b3c17733237757b78
Le-Vu-Hoang <bx1536hoang@gmail.com>
2025-03-25 21:03:28 +0700

    fix cors

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app.add_middleware(
     allow_origins=["http://localhost5173"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_header=["*"],
+    allow_headers=["*"],
 )
 
 UPLOAD_FOLDER = "uploads"
